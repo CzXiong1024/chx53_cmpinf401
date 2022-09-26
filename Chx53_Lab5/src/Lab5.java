2460b9e2ae30e4abf619a260376ba1b5804db5ba
CzXiong1024 <chx53@pitt.edu>
2022-09-25 23:53:33 -0400

    lab5-v0

---
+++ Chx53_Lab5/src/Lab5.java
@@ -0,0 +1,82 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+//Chengzhuo Xiong wrote this program on Sep25, 2022.
+
+public class Lab5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		while(true) {
+			String input = JOptionPane.showInputDialog("Please enter a positive Integer n to indicate "
+					+ "the number of times \nyou want to roll the two dices:\nn=:");
+			if(!checkPositiveInteger(input)||input==null||input.equals("")) {
+				JOptionPane.showMessageDialog(null, "Wrong format or not a positve integer, please re-enter!");
+				continue;
+			}
+			int n = Integer.valueOf(input);
+			if(n==0) {
+				JOptionPane.showMessageDialog(null, "please enter an interger greater than 0!");
+				continue;
+			}
+			Random r = new Random();
+			rollDice(n,r);
+			String ask = JOptionPane.showInputDialog("Do you wish another round of roll?\n"
+					+ "Enter 'yes' to restart, enter any other to exit!");
+			if(!ask.equalsIgnoreCase("yes")||ask==null||ask.equals("")) {
+				System.out.print("ByeBye~~!");
+				break;
+			}
+		}
+
+	}
+	
+	static void rollDice(int n, Random r) {
+		Map<Integer,Integer> map = new HashMap<>();
+		for(int i = 0; i < n; i++) {
+			int d1 = r.nextInt(1,7);
+			int d2 = r.nextInt(1,7);
+			int sum = d1 + d2;
+			if(map.containsKey(sum)) {
+				map.put(sum, map.get(sum)+1);
+			}else {
+				map.put(sum, 1);
+			}
+		}
+		String output1 = String.format("The result of n = "+n+" times of rolling two dices:\n %-8s%-18s%-13s%-19s%n"
+				,"Value","NumberOfTimes","Fraction","ExpectedFraction");
+		String output2 = "";
+		for(int i = 2; i<13 ; i++) {
+			if(map.containsKey(i)) {
+				double f = (double)map.get(i)/n;
+				double ef = 0.0;
+				if(i<=7) {
+					ef = (i-1.0)/36.0;
+				}else {
+					ef = (13.0-i)/36.0;
+				}
+				String add = String.format("    %-17d%-21d%-20.3f%-19.3f%n", i,map.get(i),f,ef);
+				output2 += add;
+			}
+		}
+		String output3 = String.format("The result of n = "+n+" times of rolling two dices:\n  %-17s%-22s%-16s%-19s%n"
+				,"Value","NumberOfTimes","Fraction","ExpectedFraction");
+		System.out.print(output3+output2+"\n");
+		JOptionPane.showMessageDialog(null, output1+output2);
+	}
+	
+	static boolean checkPositiveInteger(String in) {
+		char[] inChar = in.toCharArray();
+		for(char i : inChar) {
+			int num = (int) i ;
+			if(num<48||num>57) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
